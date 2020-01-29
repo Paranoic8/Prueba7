d25b7cddefc6a7f4b8f462035a478dc1146ea522
Shirley <pauloesmart@gmail.com>
2020-01-29 12:15:55 -0500

    segundo avance

--- app/src/main/java/rithual/company/prueba7/MainActivity.java
+++ app/src/main/java/rithual/company/prueba7/MainActivity.java
@@ -1,11 +1,17 @@
 package rithual.company.prueba7;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -28,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText edtTelefono;
     @BindView(R.id.txt_inputtelefono)
     TextInputLayout txtInputtelefono;
-    @BindView(R.id.button_save)
-    MaterialButton buttonSave;
     @BindView(R.id.container)
     LinearLayout container;
+    @BindView(R.id.button_save)
+    Button buttonSave;
+    @BindView(R.id.switch1)
+    Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +50,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @OnClick(R.id.button_save)
     public void saveData() {
 
+        View view=this.getCurrentFocus();
+        if (view!=null){
+            InputMethodManager inputMethodManager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
+
+        if (switch1.isChecked()) {
+            Toast.makeText(this, "Guardando Datos", Toast.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(container, "Aceptar terminos y condiciones", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch1.setChecked(true);
+                }
+            }).show();
+        }
     }
+
+
 }
